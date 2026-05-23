a04658539ad7fc7b60fd390a48f06fe7da55150c
Kingsley-codes <agbamkingsley@gmail.com>
2026-05-23 17:26:39 +0100

    fix

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,15 @@
+const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativewind } = require("nativewind/metro");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+config.watchFolders = [path.resolve(__dirname)];
+config.resolver = {
+  ...config.resolver,
+  unstable_enableSymlinks: true,
+  nodeModulesPaths: [path.resolve(__dirname, "node_modules")],
+};
 
 module.exports = withNativewind(config);
